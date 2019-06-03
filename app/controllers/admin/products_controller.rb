3d1254808c8a44d729b57b83ba74c18a1636bbd6
Xin <Detective_boys@hotmil.com>
2019-06-04 00:03:45 +0800

    add bootstrap to project

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,18 @@
+class Admin::ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    
+  end
+end
