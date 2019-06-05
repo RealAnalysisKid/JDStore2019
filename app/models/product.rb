090b03902d8447a6da4eb1252d5ed6c7afb5c415
Xin <Detective_boys@hotmil.com>
2019-06-06 00:16:21 +0800

    implement image upload

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,6 @@ class Product < ApplicationRecord
   validates :description, presence: true
   validates :quantity, presence: true
   validates :price, presence: true
+
+  mount_uploader :image, ImageUploader
 end
