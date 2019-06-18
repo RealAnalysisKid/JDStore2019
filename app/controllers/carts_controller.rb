c2b1f1e9643778d266beaeab31b8b080b453aef5
Xin <Detective_boys@hotmil.com>
2019-06-18 15:43:52 +0800

    add checkout routing

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,11 @@
 class CartsController < ApplicationController
+
+  def checkout
+    @cart = current_cart
+    @cart_items = current_cart.cart_items
+    @  
+  end
+
   def clean
     current_cart.clean!
     redirect_to :back, alert: "已清空购物车"
