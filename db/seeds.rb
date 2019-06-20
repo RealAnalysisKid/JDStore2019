589770228551f53c1f3ee9af54417b58ec6973c0
Xin <Detective_boys@hotmil.com>
2019-06-20 16:54:45 +0800

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,32 @@ u.password_confirmation = "111111"
 
 u.is_admin = true
 u.save
+
+
+Product.create!(title: "决断的演算",
+  description: "统计学书籍",
+  price: 134,
+  quantity: 3,
+  image: open("https://images-cn-4.ssl-images-amazon.com/images/I/51AbcO5J8DL._SX398_BO1,204,203,200_.jpg")
+)
+
+Product.create!(title: "推理的迷宫",
+  description: "逻辑悖论",
+  price: 27,
+  quantity: 31,
+  image: open("https://images-cn.ssl-images-amazon.com/images/I/51PplcfYRkL.jpg")
+)
+
+Product.create!(title: "Wild Ride",
+  description: "企业用数据发展",
+  price: 104,
+  quantity: 100,
+  image: open("https://images-cn.ssl-images-amazon.com/images/I/51-SRPs2EfL._SX325_BO1,204,203,200_.jpg")
+)
+
+Product.create!(title: "故事",
+  description: "编辑秘籍",
+  price: 43,
+  quantity: 190,
+  image: open("https://images-cn.ssl-images-amazon.com/images/I/31rYSVuqX9L.jpg")
+)
