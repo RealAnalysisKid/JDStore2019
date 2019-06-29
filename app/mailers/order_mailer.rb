9cdf31252a46ea71650528afd6ff9eabb1d225bc
Xin <Detective_boys@hotmil.com>
2019-06-29 15:14:41 +0800

    fix order#mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -15,7 +15,7 @@ class OrderMailer < ApplicationMailer
     mail(to: "admin@test.com", subject:"[JDStore] 用户#{order.user.email} 申请取消订单 #{order.token}")
   end
 
-  def notify_ship
+  def notify_ship(order)
     @order         = order
     @user          = order.user
     @product_lists = order.product_lists
@@ -23,7 +23,7 @@ class OrderMailer < ApplicationMailer
     mail(to: @user.email, subject: "[JDStore] 您的订单 #{order.token}已发货")
   end
 
-  def notify_cancel
+  def notify_cancel(order)
     @order = order
     @user = order.user
     @product_lists = order.product_lists
